af7eb98096ff1c1518748307d055579d308df57f
Adrian Rumson <rumsona42@gmail.com>
2026-02-12 09:50:44 -0400

    fixed parallelization

--- Kernels/build_kernels.sh
+++ Kernels/build_kernels.sh
@@ -4,7 +4,7 @@
 echo "Compiling Fortran kernels..."
 
 # Use gfortran + f2py
-python3 -m numpy.f2py -c pairwise_xdm.f90 -m cpair_kernel
-python3 -m numpy.f2py -c c9_loops.f90 -m c9_kernel
+python3 -m numpy.f2py -c pairwise_xdm.f90 -m cpair_kernel --f90flags="-O3 -fopenmp" -lgomp
+python3 -m numpy.f2py -c c9_loops.f90 -m c9_kernel  --f90flags="-O3 -fopenmp" -lgomp
 
 echo "Done. You should now see cpair_kernel*.so and c9_kernel*.so"
